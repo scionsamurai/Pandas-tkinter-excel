8240e51e26dcfe95246a9f468dc28e091e55ade7
scionsamurai <scionsamurai@yahoo.com>
2019-05-19 17:41:28 -0500

    Update Open_File.py

--- Open_File.py
+++ Open_File.py
@@ -1,14 +1,130 @@
 import pandas as pd
 class OpenFile:
-    def open_file(entry):
-        #for i in entries:
+    def open_file(entry, delimiter=None,header_line=0, index_col=0, chunk=None , verbose=True,terminator=None,
+                  only_col=None, dtypes=None):
         temp_field = entry.split('/')
         new_field = temp_field[(len(temp_field) - 1)]
         print('Opening ' + new_field[:-4])
+
         if entry[-4:] == '.csv':
-            data = pd.read_csv(entry, low_memory=False)
-            data.columns = [col.strip() for col in data.columns]
-            return data
+            if delimiter != None:
+                df = pd.read_csv(entry, sep=delimiter, header=None, nrows=1, low_memory=False)
+                orig_headers = df.values.tolist()
+                stripped_headers = []
+                for item in orig_headers[0]:
+                    stripped_headers.append(item.strip())
+                new_dtypes = {}
+                if dtypes != None:
+                    for key, value in dtypes.items():
+                        if key in orig_headers[0]:
+                            new_dtypes[key] = value
+                        elif key.strip() in orig_headers[0]:
+                            new_dtypes[key.strip()] = value
+                        elif key in stripped_headers:
+                            ind = stripped_headers.index(key)
+                            new_dtypes[orig_headers[0][ind]] = value
+                        elif key.strip() in stripped_headers:
+                            ind = stripped_headers.index(key.strip())
+                            new_dtypes[orig_headers[0][ind]] = value
+                        else:
+                            print(key + ':not found in ' + new_field)
+                    if new_dtypes == {}:
+                        new_dtypes = None
+                if only_col != None:
+                    new_only_col = []
+                    for item in only_col:
+                        if item in orig_headers[0]:
+                            new_only_col.append(item)
+                        elif item.strip() in orig_headers[0]:
+                            new_only_col.append(item.strip())
+                        elif item in stripped_headers:
+                            ind = stripped_headers.index(item)
+                            new_only_col.append(orig_headers[0][ind])
+                        elif item.strip() in stripped_headers:
+                            ind = stripped_headers.index(item.strip())
+                            new_only_col.append(orig_headers[0][ind])
+                        else:
+                            print(item + ':not found in ' + new_field)
+
+                    try:
+                        data = pd.read_csv(entry, sep=delimiter, header=header_line, index_col=index_col,
+                                           usecols=new_only_col, dtype=new_dtypes, verbose=verbose,
+                                           lineterminator=terminator, low_memory=False)
+                        data.columns = [col.strip() for col in data.columns]
+                        return data
+                    except ValueError as e:
+                        print(e)
+                else:
+                    try:
+                        data = pd.read_csv(entry, sep=delimiter, header=header_line, index_col=index_col,
+                                           dtype=new_dtypes, verbose=verbose, lineterminator=terminator,
+                                           low_memory=False)
+                        data.columns = [col.strip() for col in data.columns]
+                        return data
+                    except ValueError as e:
+                        print(e)
+
+            else:
+                df = pd.read_csv(entry, header=None, nrows=1, low_memory=False)
+                # print(df.values.tolist())
+                orig_headers = df.values.tolist()
+                stripped_headers = []
+                for item in orig_headers[0]:
+                    stripped_headers.append(item.strip())
+                new_dtypes = {}
+                if dtypes != None:
+                    for key, value in dtypes.items():
+                        if key in orig_headers[0]:
+                            new_dtypes[key] = value
+                        elif key.strip() in orig_headers[0]:
+                            new_dtypes[key.strip()] = value
+                        elif key in stripped_headers:
+                            ind = stripped_headers.index(key)
+                            new_dtypes[orig_headers[0][ind]] = value
+                        elif key.strip() in stripped_headers:
+                            ind = stripped_headers.index(key.strip())
+                            new_dtypes[orig_headers[0][ind]] = value
+                        else:
+                            print(key + ':not found in ' + new_field)
+                    if new_dtypes == {}:
+                        new_dtypes = None
+                if only_col != None:
+                    new_only_col = []
+                    for item in only_col:
+                        if item in orig_headers[0]:
+                            new_only_col.append(item)
+                        elif item.strip() in orig_headers[0]:
+                            new_only_col.append(item.strip())
+                        elif item in stripped_headers:
+                            ind = stripped_headers.index(item)
+                            new_only_col.append(orig_headers[0][ind])
+                        elif item.strip() in stripped_headers:
+                            ind = stripped_headers.index(item.strip())
+                            new_only_col.append(orig_headers[0][ind])
+                        else:
+                            print(item + ':not found in ' + new_field)
+
+                    try:
+                        data = pd.read_csv(entry, header=header_line, chunksize=chunk, index_col=index_col,
+                                           usecols=new_only_col, dtype=new_dtypes, verbose=verbose,
+                                           lineterminator=terminator, low_memory=False)
+                        data.columns = [col.strip() for col in data.columns]
+                        return data
+                    except ValueError as e:
+                        print(e)
+                else:
+                    try:
+                        data = pd.read_csv(entry, header=header_line, chunksize=chunk, index_col=index_col,
+                                           dtype=new_dtypes, verbose=verbose, lineterminator=terminator,
+                                           low_memory=False)
+                        data.columns = [col.strip() for col in data.columns]
+                        return data
+                    except ValueError as e:
+                        print(e)
+
+                #df = pd.read_csv(entry, header=None, nrows=1, low_memory=False)
+                #print(df.values.tolist())
+
         elif (entry[-4:] == 'xlsx') or (entry[-4:] == '.xls') or ((entry[-4:])[:3] == 'xls'):
             data = pd.read_excel(entry, sheet_name=0)
             data.columns = [col.strip() for col in data.columns]
@@ -18,8 +134,6 @@ class OpenFile:
             return data
         else:
             print('Not a valid file type...yet.')
-                # print(data)
-                # print(data.head())
     def map_func(data_frame, indexes):
         new_list = {}
         new_list.update(indexes)
@@ -27,9 +141,7 @@ class OpenFile:
             for value in data_frame[column].values:
                 if value not in indexes:
                     new_list[value] = ((len(indexes)+1) + len(new_list))
-        #temp_list = new_list.update(indexes)
         for item in new_list:
-            #print(item)
             for column in data_frame.columns.values:
                 data_frame[column].map({item:new_list[item]})
         return new_list
