e026e30fcde6cfe1672e09a57af3dc0953f347ce
scionsamurai <scionsamurai@yahoo.com>
2019-05-19 22:33:48 -0500

    Update Open_File.py

--- Open_File.py
+++ Open_File.py
@@ -70,7 +70,10 @@ class OpenFile:
                 orig_headers = df.values.tolist()
                 stripped_headers = []
                 for item in orig_headers[0]:
-                    stripped_headers.append(item.strip())
+                    try:
+                        stripped_headers.append(item.strip())
+                    except AttributeError:
+                        pass
                 new_dtypes = {}
                 if dtypes != None:
                     for key, value in dtypes.items():
