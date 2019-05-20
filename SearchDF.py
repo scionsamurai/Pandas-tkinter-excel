ee3f99e25c73f339025d1d6c16107586d89b5817
scionsamurai <scionsamurai@yahoo.com>
2019-05-19 22:03:30 -0500

    Update SearchDF.py

--- SearchDF.py
+++ SearchDF.py
@@ -1,13 +1,15 @@
 class SearchDataFrame:
     def criteria_by_column(i, search_column, search_item, new_field, int_str, data_frames):
         data = data_frames[i]
+        data[search_column] = data[search_column].str.strip()
         if int_str == 1:
             try:
                 try:
-                    rows_w_matching_crit = data[data[search_column] == search_item]
+                    rows_w_matching_crit = data[data[search_column] == str(search_item)]
                     if len(rows_w_matching_crit.values) >= 1:
                         return rows_w_matching_crit
                     else:
+                        # print('1')
                         print(search_item + ' isn\'t in ' + new_field[:-4])
                 except KeyError:
                     print(search_column + ' Header isn\'t in ' + new_field[:-4])
@@ -17,8 +19,10 @@ class SearchDataFrame:
                     if len(rows_w_matching_crit.values) >= 1:
                         return rows_w_matching_crit
                     else:
+                        # print('2')
                         print(search_item + ' isn\'t in ' + new_field[:-4])
                 except (KeyError, ValueError) as e:
+                    # print('3')
                     print(str(e)[34:] + ' isn\'t in ' + new_field[:-4])
         else:
             try:
@@ -27,15 +31,19 @@ class SearchDataFrame:
                     if len(rows_w_matching_crit.values) >= 1:
                         return rows_w_matching_crit
                     else:
+                        # print('4')
                         print(search_item + ' isn\'t in ' + new_field[:-4])
                 except KeyError:
-                    print(search_column + ' isn\'t in ' + new_field[:-4])
+                    print(search_column + ' Header isn\'t in ' + new_field[:-4])
             except FutureWarning:
                 try:
                     rows_w_matching_crit = data[data[search_column] == float(search_item)]
                     if len(rows_w_matching_crit.values) >= 1:
                         return rows_w_matching_crit
                     else:
+                        # print('5')
                         print(search_item + ' isn\'t in ' + new_field[:-4])
                 except (KeyError, ValueError) as e:
+                    # print('6')
                     print(str(e)[34:] + ' isn\'t in ' + new_field[:-4])
+
