205879f6dde181cef6d3403bfe8f9e8b85a832a3
scionsamurai <scionsamurai@yahoo.com>
2019-06-07 16:10:45 -0500

    Update Open_File.py

--- Open_File.py
+++ Open_File.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import time
 class OpenFile:
-    def open_file(entry, inp_options):
+    def __init__(self):
+        pass
+    def open_file(self, entry, inp_options):
         gen_rules = inp_options[0]
         delimiter = gen_rules['Delimiter']
         terminator = gen_rules['Terminator']
@@ -19,36 +21,30 @@ class OpenFile:
 
         if entry[-4:] == '.csv':
             if delimiter != None:
-                df = pd.read_csv(entry, sep=delimiter, header=None, nrows=1, low_memory=False)
-                orig_headers = df.values.tolist()
+                df = pd.read_csv(entry, sep=delimiter, nrows=50, low_memory=False)
+                orig_headers = df.columns.values.tolist()
                 stripped_headers = []
                 for item in orig_headers[0]:
                     try:
                         stripped_headers.append(item.strip())
                     except AttributeError:
                         stripped_headers.append(item)
-                new_dtypes = {}
+                new_dtypes = self.df_to_dtypes_dict(df)
                 if dtypes != None:
-                    if 'ALL' in dtypes:
-                        if dtypes['ALL'] == 'Text':
-                            new_dtypes = str
-                    else:
-                        for key, value in dtypes.items():
-                            # print(orig_headers[0])
-                            if key in orig_headers[0]:
-                                new_dtypes[key] = value
-                            elif key.strip() in orig_headers[0]:
-                                new_dtypes[key.strip()] = value
-                            elif key in stripped_headers:
-                                ind = stripped_headers.index(key)
-                                new_dtypes[orig_headers[0][ind]] = value
-                            elif key.strip() in stripped_headers:
-                                ind = stripped_headers.index(key.strip())
-                                new_dtypes[orig_headers[0][ind]] = value
-                            else:
-                                print(key + ':not found in ' + new_field)
-                    if new_dtypes == {}:
-                        new_dtypes = None
+                    for key, value in dtypes.items():
+                        # print(orig_headers[0])
+                        if key in orig_headers[0]:
+                            new_dtypes[key] = value
+                        elif key.strip() in orig_headers[0]:
+                            new_dtypes[key.strip()] = value
+                        elif key in stripped_headers:
+                            ind = stripped_headers.index(key)
+                            new_dtypes[orig_headers[0][ind]] = value
+                        elif key.strip() in stripped_headers:
+                            ind = stripped_headers.index(key.strip())
+                            new_dtypes[orig_headers[0][ind]] = value
+                        else:
+                            print(key + ':not found in ' + new_field)
                 if only_cols != None:
                     new_only_cols = []
                     for item in only_cols:
@@ -95,9 +91,10 @@ class OpenFile:
                         stripped_headers.append(item.strip())
                     except AttributeError:
                         stripped_headers.append(item)
-                new_dtypes = {}
+                new_dtypes = self.df_to_dtypes_dict(df)
                 if dtypes != None:
                     for key, value in dtypes.items():
+                        # print(orig_headers[0])
                         if key in orig_headers[0]:
                             new_dtypes[key] = value
                         elif key.strip() in orig_headers[0]:
@@ -110,8 +107,6 @@ class OpenFile:
                             new_dtypes[orig_headers[0][ind]] = value
                         else:
                             print(key + ':not found in ' + new_field)
-                    if new_dtypes == {}:
-                        new_dtypes = None
                 if only_cols != None:
                     new_only_cols = []
                     for item in only_cols:
@@ -146,8 +141,8 @@ class OpenFile:
                     except ValueError as e:
                         print(e)
         elif (entry[-4:] == 'xlsx') or (entry[-4:] == '.xls') or ((entry[-4:])[:3] == 'xls'):
-            df = pd.read_excel(entry, sheet_name=0, header=None, nrows=1)
-            orig_headers = df.values.tolist()
+            df = pd.read_excel(entry, sheet_name=0, nrows=50)
+            orig_headers = list(df.columns.values)#.tolist()
             stripped_headers = []
             for item in orig_headers[0]:
                 try:
@@ -155,7 +150,6 @@ class OpenFile:
                 except AttributeError:
                     stripped_headers.append(item)
             new_only_cols = []
-            new_dtypes = {}
             try:
                 for item in only_cols:
                     if item in orig_headers[0]:
@@ -170,12 +164,9 @@ class OpenFile:
                         new_only_cols.append(orig_headers[0][ind])
             except TypeError:
                 new_only_cols = None
+            new_dtypes = self.df_to_dtypes_dict(df)
             if dtypes != None:
-                if 'ALL' in dtypes:
-                    if dtypes['ALL'] == 'Text':
-                        new_dtypes = str
-                else:
-                    for key, value in dtypes.items():
+                for key, value in dtypes.items():
                         # print(orig_headers[0])
                         if key in orig_headers[0]:
                             new_dtypes[key] = value
@@ -189,8 +180,7 @@ class OpenFile:
                             new_dtypes[orig_headers[0][ind]] = value
                         else:
                             print(key + ':not found in ' + new_field)
-            if new_dtypes == {}:
-                new_dtypes = None
+
             data = pd.read_excel(entry, sheet_name=0, header=header_line, index_col=index_col,
                                  usecols=new_only_cols, dtype=new_dtypes, verbose=verbose)
             data.columns = [col.strip() for col in data.columns]
@@ -207,7 +197,7 @@ class OpenFile:
             end = time.time()
             print('-------'+ str(end-start) +'-------')
             return df_empty
-    def map_func(data_frame, indexes):
+    def map_func(self, data_frame, indexes):
         new_list = {}
         new_list.update(indexes)
         for column in data_frame.columns.values:
@@ -218,3 +208,29 @@ class OpenFile:
             for column in data_frame.columns.values:
                 data_frame[column].map({item:new_list[item]})
         return new_list
+
+    def df_to_dtypes_dict(self, df):
+        df_int = df.select_dtypes(include=['int64'])
+        converted_int2 = df_int.apply(pd.to_numeric,downcast='unsigned')
+        converted_int = converted_int2.apply(pd.to_numeric,downcast='signed')
+        df_float = df.select_dtypes(include=['float'])
+        converted_float = df_float.apply(pd.to_numeric,downcast='float')
+        df_obj = df.select_dtypes(include=['object'])
+        converted_obj = pd.DataFrame()
+        for col in df_obj.columns:
+            num_unique = len(df_obj[col].unique())
+            num_total = len(df_obj[col])
+            if num_unique / num_total < 0.5:
+                converted_obj.loc[:,col] = df_obj[col].astype('category')
+            else:
+                converted_obj.loc[:, col] = df_obj[col]
+        optimized_df = df.copy()
+        optimized_df[converted_int.columns] = converted_int
+        optimized_df[converted_float.columns] = converted_float
+        optimized_df[converted_obj.columns] = converted_obj
+
+        dtypes = optimized_df.dtypes
+        dtypes_col = dtypes.index
+        dtypes_type = [i.name for i in dtypes.values]
+        column_types = dict(zip(dtypes_col, dtypes_type))
+        return column_types
