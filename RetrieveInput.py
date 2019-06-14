5985c3de126534c87c89d0da82f9bceb03e35dee
scionsamurai <scionsamurai@yahoo.com>
2019-06-13 19:59:32 -0500

    Update RetrieveInput.py

--- RetrieveInput.py
+++ RetrieveInput.py
@@ -10,56 +10,47 @@ class Retrieve_Input:
 
     def row_frames(self,input_criteria, opened_files, data_frames, auto_open_var, output_type, NA_head_dict):
         start = time.time()
-        new_output = []
+        new_output = [] # Search results per DataFrame
         print('Searching:\n' + input_criteria[1][1].get())
         search_column = (input_criteria[0][1].get()).strip()
-        for i in range(0, len(data_frames)):
+        output_directory, zeros_dict, font_type_size, \
+        col_width, dec_rules = self.get_rules(input_criteria, search_column, output_type) # Load Settings
+        for i in range(0, len(data_frames)): # Iterate through DataFrames using i as index
             temp_field = (opened_files[i][0]).split('/')
             new_field = temp_field[(len(temp_field) - 1)]
-            if opened_files[i][2].get() == 1:
-                real_list = Split_Entry.split(input_criteria[1][1].get())
+            if opened_files[i][2].get() == 1: # If tkinter checkbutton next to file name is checked -> Open the file
+                real_list = Split_Entry.split(input_criteria[1][1].get()) # Splits main window Search Item(s) into list
                 temp_output = pd.DataFrame({'A':[]})
                 if isinstance(real_list, str) == False:
-                    try:
-                        temp_output = SearchDataFrame.criteria_by_column(search_column,real_list, new_field, 2,
-                                                                             data_frames[i]).copy()
-                        for col in NA_head_dict[opened_files[i][0]]:
-                            temp_output[col].replace(NA_head_dict[opened_files[i][0]][col], np.NaN, inplace=True)
-                        if not temp_output.empty:
-                            new_output.append(temp_output)
-                    except (TypeError, AttributeError):
-                        pass
-                        #return pd.DataFrame({'A':[]}), None
+                    func_var = 2
                 else:
-                    try:
-                        temp_output = SearchDataFrame.criteria_by_column(search_column, real_list, new_field, 1,
-                                                                         data_frames[i]).copy()
-                        for col in NA_head_dict[opened_files[i][0]]:
-                            temp_output[col].replace(NA_head_dict[opened_files[i][0]][col], np.NaN, inplace=True)
-                        if not temp_output.empty:
-                            new_output.append(temp_output)
-                    except (TypeError, AttributeError):
-                        pass
-                        #return pd.DataFrame({'A':[]}), None
+                    func_var = 1
+                try:
+                    temp_output = SearchDataFrame.criteria_by_column(search_column, real_list, new_field, func_var,
+                                                                     data_frames[i]).copy()
+                    for col in NA_head_dict[opened_files[i][0]]: # Strip space saving Filler Values from output
+                        temp_output[col].replace(NA_head_dict[opened_files[i][0]][col], np.NaN, inplace=True)
+                    if not temp_output.empty: # if Search had results add them to new_output list
+                        new_output.append(temp_output)
+                except (TypeError, AttributeError):
+                    pass
 
         try:
-            output_directory, zeros_dict, font_type_size, col_width = self.get_rules(input_criteria, search_column,
-                                                                                     output_type)
             try:
                 new_new_output = pd.concat(new_output, axis=0, sort=False, ignore_index=True)
-                for key, val in zeros_dict.items():
+                for key, val in zeros_dict.items(): # Apply Leading Zeros rules to output
                     try:
                         temp_str = '{0:0>' + str(val) + '}'
                         if str(new_new_output[key].dtype)[:3] == 'int' or \
                                 str(new_new_output[key].dtype)[:3] == 'uin' or \
-                                str(new_new_output[key].dtype)[:5] == 'float':
+                                str(new_new_output[key].dtype)[:5] == 'float': # For Numbers
                             new_new_output[key] = new_new_output[key].apply(lambda x: temp_str.format(x))
-                        else:
+                        else: # For Strings
                             try:
                                 new_new_output[key] = new_new_output[key].str.zfill(int(val))
                             except AttributeError:
                                 pass
-                    except KeyError:
+                    except KeyError: # Catch if Rule Header wasn't in output results
                         print(key + ' isn\'t in output.')
                 if output_type == 'csv':
                     new_new_output.to_csv(output_directory, index=False)
@@ -68,12 +59,16 @@ class Retrieve_Input:
                     new_new_output.to_excel(writer_orig, index=False, sheet_name='SearchOutput')
                     workbook = writer_orig.book
                     worksheet = writer_orig.sheets['SearchOutput']
-                    if font_type_size != {}:
+                    if font_type_size != {}: # Set Global Font Size / Type
                         workbook.formats[0].set_font_size(int(list(font_type_size.values())[0]))
                         workbook.formats[0].set_font_name(list(font_type_size.keys())[0])
-                    if len(col_width) > 0:
+                    if len(col_width) > 0: # Set Column / Widths
                         for rule in col_width.items():
                             worksheet.set_column(rule[0], int(rule[1]))
+                    if len(dec_rules) > 0: # Set Column / Decimal places
+                        for key, val in dec_rules.items():
+                            num_format = workbook.add_format({'num_format': val})
+                            worksheet.set_column(key, (int(list(font_type_size.values())[0])), num_format)
 
 
                     writer_orig.save()
@@ -92,7 +87,7 @@ class Retrieve_Input:
             print(str(e)[:28] + ": Close File Before Searching")
 
     def result_frames(self, data_frame, search_column, real_list, file_name):
-        new_output = []
+        new_output = [] # ^^^Function to specify rows when opening file (with main window search criteria)
         temp_field = file_name.split('/')
         new_field = temp_field[(len(temp_field) - 1)]
         if isinstance(real_list, str) == False:
@@ -132,11 +127,16 @@ class Retrieve_Input:
         try:
             output_path = var_file['dir_location']
             output_directory = os.path.join(output_path, output_dir)
-        except:
+        except KeyError:
             output_directory = output_dir
         try:
             font_rules = var_file['font_rules']
         except KeyError:
             font_rules = {}
+        try:
+            dec_rules = var_file['decimal_places']
+        except KeyError:
+            dec_rules = {}
+
         var_file.close()
-        return output_directory, zeros_dict, font_rules, col_width
+        return output_directory, zeros_dict, font_rules, col_width, dec_rules
