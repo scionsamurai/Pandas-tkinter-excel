b8a24893ac2225cd3d103a82df8da3c7c73d3796
scionsamurai <scionsamurai@yahoo.com>
2019-06-16 19:31:04 -0500

    Update SplitEntry.py

--- SplitEntry.py
+++ SplitEntry.py
@@ -4,6 +4,8 @@ class Split_Entry:
             xlist = list(dict.fromkeys(xlist))
             if '' in split_l:
                 xlist.remove('')
+            if len(xlist) == 1:
+                xlist = xlist[0]
             return xlist
         entry = entry
         if len(entry.split('\t')) > 1 and len(entry.split('\n')) > 1:
@@ -19,14 +21,14 @@ class Split_Entry:
             t_split_l = entry.split('\t')
             split_l = []
             for t_split in t_split_l:
-                split_l.append(t_split)
+                split_l.append(t_split.strip())
             split_l = remove_dups(split_l)
             return split_l
         elif len(entry.split('\n')) > 1:
             n_split_l = entry.split('\n')
             split_l = []
             for n_split in n_split_l:
-                split_l.append(n_split)
+                split_l.append(n_split.strip())
             split_l = remove_dups(split_l)
             return split_l
         else:
